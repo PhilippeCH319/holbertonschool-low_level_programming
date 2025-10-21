a9ad2e5ea780215e379f85c655eac0fd857abf60
PhilippeCH319 <12068@holbertonstudents.com>
2025-10-21 13:21:03 +0000

    4-puts.c

---
+++ 4-puts.c
@@ -0,0 +1,19 @@
+/*
+ * File: 4-puts.c
+ * Auth: PhilippeCH319 <12068@holbertonstudents.com>
+ */
+
+#include <stdio.h>
+
+/**
+ * main - Prints "Programming is like building a multilingual"
+ *                puzzle, followed by a new line.
+ *
+ * Return: Always 0.
+ */
+ int main(void)
+ {
+    puts("\"Programmingis like building a multiligual puzzle")
+
+    return (0) ;
+ }
